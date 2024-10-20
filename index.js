fa7e92cff7dc24212a262734754c3475622835c7
Divine <divineanyanwu123@gmail.com>
2024-10-20 23:48:09 +0100

    added moment timezone to the html head and updates the application to display current time in the location

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // alert(`Hello`);
+function updateTime() {
+  let losAngeles = document.querySelector("#los-angeles");
+
+  let dateLosAng = losAngeles.querySelector(".date");
+  let timeLosAng = losAngeles.querySelector(".time");
+  let losAngTime = moment().tz("America/Los_Angeles");
+
+  dateLosAng.innerHTML = losAngTime.format("MMMM Do YYYY");
+  timeLosAng.innerHTML = losAngTime.format("h:mm:ss [<small>]A[</small>]");
+
+  let newParis = document.querySelector("#paris");
+
+  let dateParis = newParis.querySelector(".date");
+  let timeParis = newParis.querySelector(".time");
+  let parisTime = moment().tz("America/Los_Angeles");
+
+  dateParis.innerHTML = parisTime.format("MMMM Do YYYY");
+  timeParis.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+}
+setInterval(updateTime, 1000);
