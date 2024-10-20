9260f55291fd667dcd47f20556c0b240066ddd02
Divine <divineanyanwu123@gmail.com>
2024-10-21 00:49:44 +0100

    added the current location

--- index.js
+++ index.js
@@ -33,6 +33,10 @@ function updateCity(event) {
     "A"
   )}</small></div>
       </div>`;
+
+  if (newTimeZone === current) {
+    newTimeZone = moment.tz.guess();
+  }
 }
 
 let selectCity = document.querySelector("#search-city");
